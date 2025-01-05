f0fc9ed36ca6cc7aaa2f903c0b1e044690731fdb
Nap20192 <kogay20192@gmail.com>
2025-01-05 18:56:39 +0500

    unplash

--- index.js
+++ index.js
@@ -25,14 +25,16 @@ app.get('/', (req,res)=>{
 
 app.post('/weather', async (req, res) => {
     const city =req.body.city; 
+
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
     try {
         const response = await axios.get(url);
         const weatherData = response.data;
-        res.render('weather',{weatherData,
-            "lat":weatherData.lon,
-            log:weatherData.log
-        })
+
+        const photo = await axios.get(`https://api.unsplash.com/search/photos?query=${city} ${weatherData.weather[0].description}&client_id=yXWN-B1ouwIhIHNGXMFTnnuMRRtM0G6GQwbrmm_DAHg`)
+        const p = photo.data.results[0]
+        console.log(p); 
+        res.render('weather',{weatherData,p})
     } catch (error) {
         console.error(error);
         res.status(500).send('Ошибка при получении данных от OpenWeatherAPI.');
